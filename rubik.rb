34d39c6480a33de96de6a62379f7739644a3345d
Matt Neary <neary.matt@gmail.com>
2013-06-23 18:13:40 -0400

    permutation of all faces, rather than merely each cubie.

--- rubik.rb
+++ rubik.rb
@@ -1,3 +1,19 @@
+class Proc
+	# modify the lambda class.
+	def self.compose(f, g)
+		# provide a function for composition.
+		lambda { |*args| f[g[*args]] }
+	end
+	def *(g)
+		# provide an infix shorthand with reversed order, to...
+		# ... make writing of algorithms natural.
+		Proc.compose(g, self)
+	end
+	def inv
+		self * self * self
+	end
+end
+
 class Cube
 	attr_accessor :cube, :aliases
 	attr_accessor :_L, :_R
@@ -11,14 +27,16 @@ class Cube
 	end
 	
 	def initialize
+		# set instance variables, including cubie naming, initial cube, ...
+		# ... and the variuous faces to each cubie (not yet put to use).
 		@_L, @_R = 0, 1
 		@U, @D, @L, @R, @F, @B = 0, 1, 2, 3, 4, 5
 		@cube = [makeFace(@U), 
-				 makeFace(@D), 
-				 makeFace(@L), 
-				 makeFace(@R), 
-				 makeFace(@F), 
-				 makeFace(@B)]
+			 makeFace(@D), 
+			 makeFace(@L), 
+			 makeFace(@R), 
+			 makeFace(@F), 
+			 makeFace(@B)]
 		
 		@aliases = [
 			[[:U, :U, :_L],[:L, :U, :_L],[:B, :U, :_R]],
@@ -39,15 +57,13 @@ class Cube
 	end
 	
 	def getCubie(accessor)
+		# take a cubie name (e.g., [@L, @U, @_L]) and find cubie...
+		# ... value at that position.
 		@cube[accessor[0]][accessor[1]][accessor[2]]
 	end
 	
-	def setCubie(accessor, value)
-		@aliases.select { |cubies|
-			cubies.include? accessor
-		}.first.each { |cubie|
-			@cube[cubie[0]][cubie[1]][cubie[2]] = value
-		}
+	def setCubie(cubie, value)
+		@cube[cubie[0]][cubie[1]][cubie[2]] = value
 	end
 	
 	def permute(permutation)
@@ -65,15 +81,12 @@ class Cube
 		return self
 	end
 	
-	def invert(permutation)
-		permute(permutation).permute(permutation).permute(permutation)
-	end
-	
 	def cubie(face, y, x)
 		[send(face), send(y), send(x)]
 	end
 	
 	def to_s
+		# render a cube as a table.
 		names = ["U", "D", "L", "R", "F", "B"]
 		_names = ["L", "R"]
 		"\n+---------+\n" + @cube.map { |face|
@@ -86,6 +99,7 @@ class Cube
 	end
 	
 	def join_to_s(other)
+		# render two cubes side by side in a table.
 		names = ["U", "D", "L", "R", "F", "B"]
 		_names = ["L", "R"]
 		
@@ -106,34 +120,98 @@ class Cube
 			}.join(" | \n| ") + ' |'			
 		}.join(separatorLine) + headerLine
 	end
-	
-	def faceTurn(face)
-		[
+
+	def formPermutation(permutation, cube)
+		# map symbolic cubie name, e.g., [:L, :U, :_L] to a list...
+		# ... of indices, e.g., [@L, @U, @_L].
+		permutation.map { |cubie|
+			cubie.map { |name|
+				cube.send name
+			}
+		}
+	end
+
+	def faceTurn(face, cube)
+		# form the permutation corresponding to a turn of a given...
+		# ... face. this permutation includes a transpose of the...
+		# ... turned face and two cyclical permutations on adjacent...
+		# ... faces, as well.
+
+		# transpose...
+		cube.permute formPermutation([
 			[face, :U, :_L], 
 			[face, :U, :_R], 
 			[face, :D, :_R], 
 			[face, :D, :_L]
-		].map { |cubie|
-			cubie.map { |name|
-				send name
-			}
-		}
+		], cube)
+
+		# permute adjacent faces
+		permutationFar = []
+		permutationClose = []
+		if face == :U or face == :D
+			permutationFar = [
+				[:L, face, :_L],
+				[:F, face, :_L],
+				[:R, face, :_L],
+				[:B, face, :_L]
+			]
+			permutationClose = [
+				[:L, face, :_R],
+				[:F, face, :_R],
+				[:R, face, :_R],
+				[:B, face, :_R]
+			]
+		elsif face == :L or face == :R
+			third = face == :L ? :_L : :_R
+			permutationFar = [
+				[:U, :U, third],
+				[:F, :U, third],
+				[:D, :U, third],
+				[:B, :D, third==:_L ? :_R : :_L],
+			]
+			permutationClose = [
+				[:U, :D, third],
+				[:F, :D, third],
+				[:D, :D, third],
+				[:B, :U, third==:_L ? :_R : :_L]
+			]
+		elsif face == :F or face == :B
+			permutationFar = [
+				[:U, face == :F ? :D : :U, face == :F ? :_L : :_L],
+				[:R, face == :F ? :U : :U, face == :F ? :_L : :_R],
+				[:D, face == :F ? :U : :D, face == :F ? :_L : :_L],
+				[:L, face == :F ? :D : :D, face == :F ? :_R : :_L],
+			]
+			permutationClose = [
+				[:U, face == :F ? :D : :U, face == :F ? :_R : :_R],
+				[:R, face == :F ? :D : :D, face == :F ? :_L : :_R],
+				[:D, face == :F ? :U : :D, face == :F ? :_R : :_R],
+				[:L, face == :F ? :U : :U, face == :F ? :_R : :_R]
+			]
+		end
+
+		cube.permute formPermutation(permutationFar, cube)
+		cube.permute formPermutation(permutationClose, cube)
+
+		cube
 	end
 end
 
+# form two cubes, one for manipulation and one for comparison.
 cube = Cube.new()
 defCube = Cube.new()
 
-U = cube.faceTurn :U
-D = cube.faceTurn :D
-L = cube.faceTurn :L
-R = cube.faceTurn :R
-F = cube.faceTurn :F
-B = cube.faceTurn :B
+# make faceturn lambdas of our cube to be manipulated.
+U = lambda { |cube| cube.faceTurn :U, cube }
+D = lambda { |cube| cube.faceTurn :D, cube }
+L = lambda { |cube| cube.faceTurn :L, cube }
+R = lambda { |cube| cube.faceTurn :R, cube }
+F = lambda { |cube| cube.faceTurn :F, cube }
+B = lambda { |cube| cube.faceTurn :B, cube }
 
-cube.permute L
-cube.invert U
-cube.invert R
-cube.permute U
+# perform an algorithm:
+# 	L U' R' U L' U R U
+(L * U.inv * R.inv * U * L.inv * U * R * U)[cube]
 
+# print our results compared to our default cube in a table.
 print defCube.join_to_s(cube)
